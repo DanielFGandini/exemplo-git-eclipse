d8f232fc63cb17d03b3e17542bc6dea14ee89817
Daniel Fernandes Gandini <danielgandini77@gmail.com>
2019-11-20 17:55:47 -0300

    Alterando

--- Testando/src/Git/Teste.java
+++ Testando/src/Git/Teste.java
@@ -3,7 +3,7 @@ package Git;
 public class Teste {
 	public static void main(String[] args) {
 		System.out.println("Olá Git");
-		System.out.println("Alou");
+		System.out.println("Alou,Alou");
 		
 	}
 }
